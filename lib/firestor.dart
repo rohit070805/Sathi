956800981d3bff1730b7e422288cff34a4b2f7db
Rohit Kumar <149245736+rohit070805@users.noreply.github.com>
2024-11-16 13:37:42 +0530

    Update firestor.dart

--- lib/firestor.dart
+++ lib/firestor.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sathi/userModal.dart';
+import 'package:uuid/uuid.dart';
 
 class Firebase_firestor{
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
@@ -14,5 +16,38 @@ class Firebase_firestor{
     });
     return true;
   }
+  Future<Usermodel> getUser()async{
+    try{
+      final user = await _firebaseFirestore.collection('users').doc(_auth.currentUser!.uid).get();
+      final snapuser = user.data()!;
+      return Usermodel(snapuser['username'], snapuser['email'], snapuser['followers'], snapuser['following']);
+    }on FirebaseException catch(e){
+      throw e.message.toString();
+    }
+  }
+  Future<bool> CreatePost({
+    required String starting,
+    required String destination,
+    required DateTime startdate,
+    required DateTime endDate,
+    required int members
+})async{
+    var uid = Uuid().v4();
+    DateTime data = new DateTime.now();
+    Usermodel user = await getUser();
+    await _firebaseFirestore.collection('posts').doc(uid).set({
+      'Starting':starting,
+      'Destination':destination,
+      'StartDate':startdate,
+      'EndDate':endDate,
+      'members':members,
+      'username':user.username,
+      'postId':uid,
+      'time':data,
+      'uid':_auth.currentUser!.uid
+    });
+    return true;
+  }
+
 
 }
